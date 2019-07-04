e4936a8fd67866d34af36b50a0699147b70207f2
Artem Ignatovich <a.ignatovich@itransition.com>
2019-07-04 18:13:36 +0300

    C# 8.0 nullable reference

---
+++ CSharpFeatures/8.0/NullableReferenceTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NullableReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = null;
+
+            // Will not raise Null ref exception
+            Console.WriteLine(s[0]);
+        }
+    }
+}
